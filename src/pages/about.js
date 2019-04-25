659acff5ca28ecb619be62106f1cbb14be085de2
Kevin Yan <k3vinyan@gmail.com>
2019-04-25 15:45:44 -0700

    include short bio, still needs work

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,17 +1,25 @@
 import React from 'react';
 
-import NavList from '../components/navlist';
+import Navlist from '../components/navlist';
+
+import aboutStyles from '../styles/about.module.scss';
+
 
 const About = () => {
 
     return (
-        <div>
-            Web Development
-            k3vinyan@gmail.com
-            4089604418
+        <div className={aboutStyles.container} id="about">
+            <div className={aboutStyles.firstColumn}>
+                <h1>SVG About</h1>
+                <div><Navlist /></div>
+            
+            </div>
 
-            linkedln
-            <NavList />
+            <div className={aboutStyles.secondColumn}>
+                <p>Hello! I am a web developering living in San Jose, California</p>
+                I am a web developer that is interested in creating beautiful web applications and solving challenging problems. On the journey, I have established a good foundation with full stack development but generally found myself fully immerse and passion with front-end development. On my free time I am always looking to improve myself with new technologies and projects.
+            </div>
+            
         </div>
     )
 };
