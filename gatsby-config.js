e921b3994be3fe26ec15a15a48a3da93da12654b
Kevin Yan <k3vinyan@gmail.com>
2019-04-26 17:39:11 -0700

    include gatsby-react-svg plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,13 @@
 module.exports = {
     plugins: [
-        `gatsby-plugin-sass`
+        `gatsby-plugin-sass`,
+        {
+            resolve: "gatsby-plugin-react-svg",
+            options: {
+                rule: {
+                    include: /public/
+                }
+            }
+        }
     ]
 }
