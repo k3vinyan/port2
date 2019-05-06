d24b97077f1128291721cf5dc9abcdfe17f694ec
Kevin Yan <k3vinyan@gmail.com>
2019-05-06 08:54:48 -0700

    add div container to center text elements

--- src/pages/project.js
+++ src/pages/project.js
@@ -14,26 +14,29 @@ const Project = () => {
             </div>
 
             <div className={ProjectStyles.secondColumn}>
-                <div className={ProjectStyles.projectContainer} >
-                    <div><h1 className={ProjectStyles.positionTitle}>DSF3-Flex | Full Stack Developer</h1></div>
-                    <div><p>Web application developed for an undisclosed company to help track, organize, onboard, and release incoming and outgoing drivers</p></div>
-                    <div><a href='https://github.com/k3vinyan/DSF3-Flex' rel="noopener noreferrer" target="_blank" className={ProjectStyles.link}>Github</a></div>
-                    <div><a className={ProjectStyles.link}><strike>App</strike></a><i className={ProjectStyles.italic}>  (site no longer operational)</i></div>
-                </div>
+                <div>
+                    <div className={ProjectStyles.projectContainer} >
+                        <div><h1 className={ProjectStyles.positionTitle}>DSF3-Flex | Full Stack Developer</h1></div>
+                        <div><p>Web application developed for an undisclosed company to help track, organize, onboard, and release incoming and outgoing drivers</p></div>
+                        <div><a href='https://github.com/k3vinyan/DSF3-Flex' rel="noopener noreferrer" target="_blank" className={ProjectStyles.link}>Github</a></div>
+                        <div><a className={ProjectStyles.link}><strike>App</strike></a><i className={ProjectStyles.italic}>  (site no longer operational)</i></div>
+                    </div>
 
-                <div className={ProjectStyles.projectContainer} >
-                    <div><h1 className={ProjectStyles.positionTitle}>Hack1n-Slash | Stack Developer</h1></div>
-                    <div><p>Gamified coding application that allows users to improve their accuracy and speed while writing code based offof code samples fetched from public GitHub repositories</p></div>
-                    <div><a href='https://github.com/hdngo/TeamHACKIN' rel="noopener noreferrer" target="_blank" className={ProjectStyles.link}>Github</a></div>
-                    <div><a href='https://hack1n-slash.herokuapp.com/' rel="noopener noreferrer" target="_blank" className={ProjectStyles.link}>App</a></div>
-                </div>
+                    <div className={ProjectStyles.projectContainer} >
+                        <div><h1 className={ProjectStyles.positionTitle}>Hack1n-Slash | Stack Developer</h1></div>
+                        <div><p>Gamified coding application that allows users to improve their accuracy and speed while writing code based offof code samples fetched from public GitHub repositories</p></div>
+                        <div><a href='https://github.com/hdngo/TeamHACKIN' rel="noopener noreferrer" target="_blank" className={ProjectStyles.link}>Github</a></div>
+                        <div><a href='https://hack1n-slash.herokuapp.com/' rel="noopener noreferrer" target="_blank" className={ProjectStyles.link}>App</a></div>
+                    </div>
 
-                <div className={ProjectStyles.projectContainer} >
-                    <div><h1 className={ProjectStyles.positionTitle}>Car Wars | Full Stack Developer</h1></div>
-                    <div><p>A unique card game that borrows elements of the classic card game War</p></div>
-                    <div><a href='https://github.com/k3vinyan/CardsWar' rel="noopener noreferrer" target="_blank" className={ProjectStyles.link}>Github</a></div>
-                    <div><a href='https://cardswar.herokuapp.com/' rel="noopener noreferrer" target="_blank" className={ProjectStyles.link}>App</a></div>
+                    <div className={ProjectStyles.projectContainer} >
+                        <div><h1 className={ProjectStyles.positionTitle}>Car Wars | Full Stack Developer</h1></div>
+                        <div><p>A unique card game that borrows elements of the classic card game War</p></div>
+                        <div><a href='https://github.com/k3vinyan/CardsWar' rel="noopener noreferrer" target="_blank" className={ProjectStyles.link}>Github</a></div>
+                        <div><a href='https://cardswar.herokuapp.com/' rel="noopener noreferrer" target="_blank" className={ProjectStyles.link}>App</a></div>
+                    </div>
                 </div>
+                
             </div>
         </div>
     )
